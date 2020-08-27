4f431e59790c186a1d4464f3540649f7ad00c50a
guang wu <guangwu@warg.local>
2020-08-27 13:54:03 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,6 @@ module.exports = {
     "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
     quotes: [1, "single"],
     semi: 0,
+    "no-unused-vars": "warn", //把该条提示信息转换成警告信息
   },
 };
